02c3d9ea8a0f5cf4e73bc7703edccc42fe18523f
Tanjeer Bin Haque <tbh.tilok@live.com>
2021-06-20 00:59:31 +0600

    committed with adding bootstrap in client angular app

--- src/Mono2Micro.Client.App/client-app/src/app/app.module.ts
+++ src/Mono2Micro.Client.App/client-app/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule.forRoot()
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
